88f875335ae03df37743aa4b6acdaa520318b0a6
Kasyaakk <kalamkas.imasheva@studio.unibo.it>
2024-05-26 20:14:33 +0200

    Functions to prepare data and to train models

--- investment-calculator.py
+++ investment-calculator.py
@@ -14,3 +14,19 @@ def load_data():
         'inflation_rate': np.random.uniform(0.01, 0.03, 120)
     })
     return data
+
+# Function to prepare data
+def prepare_data(data):
+    data['date_num'] = data['date'].apply(lambda x: x.toordinal())
+    X = data[['date_num']]
+    y_eur_to_usd = data['eur_to_usd']
+    y_inflation_rate = data['inflation_rate']
+    return X, y_eur_to_usd, y_inflation_rate
+
+# Function to train models
+def train_models(X, y_eur_to_usd, y_inflation_rate):
+    model_eur_to_usd = LinearRegression()
+    model_eur_to_usd.fit(X, y_eur_to_usd)
+    model_inflation_rate = LinearRegression()
+    model_inflation_rate.fit(X, y_inflation_rate)
+    return model_eur_to_usd, model_inflation_rate
